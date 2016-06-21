922c7f350ce9ba1e99b1bd7a14990128b1e38ba3
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-06-21 12:08:20 -0400

    Fix run_local.bat
    Fix comment markers that weren't. Fix exit mechanism to not terminate the
    hosting command prompt.

--- run_local.bat
+++ run_local.bat
@@ -1,9 +1,10 @@
-# This is a sample Windows batch script to run ctest_driver_script.cmake locally
-# without Jenkins.
-# 1. Update variables in this script to build you want, including the path to
-#    the cmake executable.
-# 2. Clone drake into the path specified in %WORKSPACE%.
-# 3. Run this script.
+REM This is a sample Windows batch script to run ctest_driver_script.cmake
+REM locally
+REM without Jenkins.
+REM 1. Update variables in this script to build you want, including the path to
+REM    the cmake executable.
+REM 2. Clone drake into the path specified in %WORKSPACE%.
+REM 3. Run this script.
 
 SET compiler = msvc-ninja-64
 SET debug = false
@@ -20,12 +21,12 @@ SET PATH = "C:\Program Files (x86)\CMake\bin;%PATH%"
 
 ctest -Dbuildname=%JOB_NAME% -Dsite=%NODE_NAME% -S %WORKSPACE%/ci/ctest_driver_script.cmake --extra-verbose --output-on-failure
 if EXIST %WORKSPACE%/FAILURE (
-  EXIT 1
+  EXIT /B 1
 )
 if EXIST %WORKSPACE%/SUCCESS (
-  EXIT 0
+  EXIT /B 0
 )
 if EXIST %WORKSPACE%/UNSTABLE (
-  EXIT 0
+  EXIT /B 0
 )
-EXIT 1
+EXIT /B 1
