1902ae33377781ae95c74d796bae9711fd5ecc38
Jamie Snape <jamie.snape@kitware.com>
2016-06-21 12:17:29 -0400

    Fix exit calls in ctest_driver_script_wrapper.bat

--- ctest_driver_script_wrapper.bat
+++ ctest_driver_script_wrapper.bat
@@ -2,12 +2,12 @@ ECHO ON
 SET PATH=C:\Program Files (x86)\CMake\bin;%PATH%
 ctest -Dbuildname=%JOB_NAME% -Dsite=%NODE_NAME% -S %WORKSPACE%/ci/ctest_driver_script.cmake --extra-verbose --no-compress-output --output-on-failure
 if EXIST %WORKSPACE%/FAILURE (
-  EXIT 1
+  EXIT /B 1
 )
 if EXIST %WORKSPACE%/SUCCESS (
-  EXIT 0
+  EXIT /B 0
 )
 if EXIST %WORKSPACE%/UNSTABLE (
-  EXIT 0
+  EXIT /B 0
 )
-EXIT 1
+EXIT /B 1
