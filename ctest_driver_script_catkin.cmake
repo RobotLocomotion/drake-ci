f5108f4ec77468f0b8d324281fe57036dde76d36
John R. Hoare <john.hoare@kitware.com>
2016-07-19 18:30:37 +0000

    Updated to create symbolic link and walk through ros packages.

--- ctest_driver_script_catkin.cmake
+++ ctest_driver_script_catkin.cmake
@@ -59,12 +59,17 @@ message("
 ctest_start("${DASHBOARD_MODEL}" TRACK "${DASHBOARD_TRACK}" QUIET)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" QUIET)
 
+set(CTEST_CONFIGURE_COMMAND "cmake -E create_symlink ${DASHBOARD_WORKSPACE}/src/drake/ros ${DASHBOARD_WORKSPACE}/src/drake_ros_integration")
+ctest_configure(BUILD "${DASHBOARD_WORKSPACE}"
+                SOURCE "${DASHBOARD_WORKSPACE}"
+                RETURN_VALUE DASHBOARD_SUPERBUILD_CONFIGURE_RETURN_VALUE QUIET)
+
 set(CTEST_CONFIGURE_COMMAND "catkin init")
 ctest_configure(BUILD "${DASHBOARD_WORKSPACE}"
                 SOURCE "${DASHBOARD_WORKSPACE}"
                 RETURN_VALUE DASHBOARD_SUPERBUILD_CONFIGURE_RETURN_VALUE QUIET)
 
-set(CTEST_BUILD_COMMAND "catkin build --no-status -v -DCMAKE_BUILD_TYPE=RelWithDebInfo")
+set(CTEST_BUILD_COMMAND "catkin build --no-status -v -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCATKIN_ENABLE_TESTING=True")
 ctest_build(BUILD "${DASHBOARD_WORKSPACE}" APPEND
   NUMBER_ERRORS DASHBOARD_SUPERBUILD_NUMBER_BUILD_ERRORS QUIET)
 ctest_submit(PARTS Build)
@@ -79,6 +84,21 @@ ctest_test(BUILD "${DASHBOARD_WORKSPACE}/build/drake/drake" ${CTEST_TEST_ARGS}
   RETURN_VALUE DASHBOARD_TEST_RETURN_VALUE QUIET APPEND)
 ctest_submit(PARTS Test)
 
-ctest_test(BUILD "${DASHBOARD_WORKSPACE}/build/drake_ros" ${CTEST_TEST_ARGS}
-  RETURN_VALUE DASHBOARD_TEST_RETURN_VALUE QUIET APPEND)
-ctest_submit(PARTS Test)
+# Run tests on every ROS Package in the workspace
+execute_process(COMMAND catkin list -u
+                WORKING_DIRECTORY ${DASHBOARD_WORKSPACE}
+                RESULT_VARIABLE RUN_RESULT
+                OUTPUT_VARIABLE ROS_PACKAGES
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+# Replace newlines with ; to turn output into a list
+string(REPLACE "\n" ";" ROS_PACKAGES_LIST "${ROS_PACKAGES}")
+
+# Loop through all detected packages and run tests
+foreach(PKG ${ROS_PACKAGES_LIST})
+  if (NOT ${PKG} STREQUAL "drake")
+    #message("Testing ROS Package: ${PKG}")
+    ctest_test(BUILD "${DASHBOARD_WORKSPACE}/build/${PKG}" ${CTEST_TEST_ARGS}
+      RETURN_VALUE DASHBOARD_TEST_RETURN_VALUE QUIET APPEND)
+    ctest_submit(PARTS Test)
+  endif()
+endforeach()
