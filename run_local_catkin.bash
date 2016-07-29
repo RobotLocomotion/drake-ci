278935203344a3f148e6ed084ebf02af55dd768a
Jamie Snape <jamie.snape@kitware.com>
2016-07-29 13:40:22 -0400

    Fix run_local_catkin

--- run_local_catkin.bash
+++ run_local_catkin.bash
@@ -22,5 +22,5 @@ export WORKSPACE="${HOME}/workspace/unix-experimental"
 
 export PATH="/usr/local/bin:${PATH}"
 
-ctest -Dbuildname="${JOB_NAME}" -Dsite="${NODE_NAME}" -S "${WORKSPACE}/ci/ctest_driver_script.cmake" --extra-verbose --output-on-failure
+ctest -Dbuildname="${JOB_NAME}" -Dsite="${NODE_NAME}" -S "${WORKSPACE}/ci/ctest_driver_script_catkin.cmake" --extra-verbose --output-on-failure
 [[ (-e "${WORKSPACE}/SUCCESS" || -e "${WORKSPACE}/UNSTABLE") && ! -e "${WORKSPACE}/FAILURE" ]]
