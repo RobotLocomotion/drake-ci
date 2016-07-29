115de896852458c15bff9ba4f888a5391217653b
Jamie Snape <jamie.snape@kitware.com>
2016-07-29 13:35:49 -0400

    Add WITH_SPDLOG and update run_local

--- run_local_catkin.bash
+++ run_local_catkin.bash
@@ -4,21 +4,19 @@
 # without Jenkins.
 # 1. Update variables in this script to build you want, including the path to
 #    the cmake executable.
-# 2. Clone drake into the path specified in $WORKSPACE.
+# 2. Clone drake into $WORKSPACE/src/drake.
 # 3. Run this script.
 
 export compiler=gcc
 export coverage=false
 export debug=false
-export documentation=false
 export matlab=false
-export minimal=false
 export openSource=true
-export ros=false
+export ros=true
 export track=experimental
 
 export BUILD_ID=0
-export JOB_NAME="unix-${compiler}-experimental"
+export JOB_NAME="unix-${compiler}-experimental-ros"
 export NODE_NAME=$(hostname -s)
 export WORKSPACE="${HOME}/workspace/unix-experimental"
 
