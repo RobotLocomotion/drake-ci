6e1016cd3915b7b462a8d45665a4f2710d76b167
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-08-26 17:20:27 -0400

    Factor out site and build information
    Reorder the various bits of logic to set up the site and build identity
    information and more them to a separate file. Move some other general
    setup logic closer to the beginning of the CI script, rather than being
    randomly interspersed with other logic.

---
+++ driver/site.cmake
@@ -0,0 +1,67 @@
+# Set site
+if(DEFINED site)
+  if(APPLE)
+    string(REGEX REPLACE "(.*)_(.*)" "\\1" DASHBOARD_SITE "${site}")
+  elseif(NOT WIN32)
+    string(REGEX REPLACE "(.*) (.*)" "\\1" DASHBOARD_SITE "${site}")
+  else()
+    set(DASHBOARD_SITE "${site}")
+  endif()
+  set(CTEST_SITE "${DASHBOARD_SITE}")
+else()
+  message(WARNING "*** CTEST_SITE was not set")
+endif()
+
+# Set build track
+if(NOT TRACK)
+  set(TRACK "experimental")
+endif()
+
+# Set build name
+if(DEFINED buildname)
+  set(CTEST_BUILD_NAME "${buildname}")
+  if(TRACK STREQUAL "experimental")
+    if(DEBUG)
+      set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-debug")
+    else()
+      set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-release")
+    endif()
+  endif()
+else()
+  message(WARNING "*** CTEST_BUILD_NAME was not set")
+endif()
+
+# set model and track for submission
+set(DASHBOARD_MODEL "Experimental")
+if(TRACK STREQUAL "continuous")
+  set(DASHBOARD_TRACK "Continuous")
+elseif(TRACK STREQUAL "nightly")
+  set(DASHBOARD_MODEL "Nightly")
+  set(DASHBOARD_TRACK "Nightly")
+else()
+  set(DASHBOARD_TRACK "Experimental")
+endif()
+
+# Set build id
+if(DEFINED ENV{BUILD_ID})
+  set(DASHBOARD_LABEL "jenkins-${CTEST_BUILD_NAME}-$ENV{BUILD_ID}")
+  set_property(GLOBAL PROPERTY Label "${DASHBOARD_LABEL}")
+else()
+  message(WARNING "*** ENV{BUILD_ID} was not set")
+  set(DASHBOARD_LABEL "")
+endif()
+
+# Set pull request id
+if(DEFINED ENV{ghprbPullId})
+  set(CTEST_CHANGE_ID "$ENV{ghprbPullId}")
+  set(DASHBOARD_CHANGE_TITLE "$ENV{ghprbPullTitle}")
+  string(LENGTH "${DASHBOARD_CHANGE_TITLE}" DASHBOARD_CHANGE_TITLE_LENGTH)
+  if(DASHBOARD_CHANGE_TITLE_LENGTH GREATER 30)
+    string(SUBSTRING "${DASHBOARD_CHANGE_TITLE}" 0 27
+      DASHBOARD_CHANGE_TITLE_SUBSTRING)
+    set(DASHBOARD_CHANGE_TITLE "${DASHBOARD_CHANGE_TITLE_SUBSTRING}...")
+  endif()
+  set(DASHBOARD_BUILD_DESCRIPTION
+    "*** Build Description: <a title=\"$ENV{ghprbPullTitle}\" href=\"$ENV{ghprbPullLink}\">PR ${CTEST_CHANGE_ID}</a>: ${DASHBOARD_CHANGE_TITLE}")
+  message("${DASHBOARD_BUILD_DESCRIPTION}")
+endif()
