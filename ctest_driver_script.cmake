b909ea292df39ff5e9bceafa31f5dd00e82493ae
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-11-04 16:03:20 -0400

    Split more setup to a separate file
    Move the logic to copy information from the environment and set up the
    general environmental configuration (particularly, the workspace
    location) to a separate file.

--- ctest_driver_script.cmake
+++ ctest_driver_script.cmake
@@ -33,56 +33,7 @@ set(DASHBOARD_TOOLS_DIR ${CMAKE_CURRENT_LIST_DIR}/tools)
 set(DASHBOARD_TEMPORARY_FILES "")
 
 include(${DASHBOARD_DRIVER_DIR}/functions.cmake)
-
-# Set default compiler and generator (if not specified) or copy from environment
-if(NOT DEFINED ENV{compiler})
-  if(APPLE)
-    message(WARNING "*** ENV{compiler} was not set; defaulting to 'clang'")
-    set(COMPILER "clang")
-  else()
-    message(WARNING "*** ENV{compiler} was not set; defaulting to 'gcc'")
-    set(COMPILER "gcc")
-  endif()
-else()
-  set(COMPILER $ENV{compiler})
-endif()
-if(NOT DEFINED ENV{generator})
-  message(WARNING "*** ENV{generator} was not set; defaulting to 'make'")
-  set(GENERATOR "make")
-else()
-  set(GENERATOR $ENV{generator})
-endif()
-
-# Copy remaining configuration from environment
-set(COVERAGE $ENV{coverage})
-set(DEBUG $ENV{debug})
-set(DOCUMENTATION $ENV{documentation})
-set(MATLAB $ENV{matlab})
-set(MEMCHECK $ENV{memcheck})
-set(MINIMAL $ENV{minimal})
-set(OPEN_SOURCE $ENV{openSource})
-set(PROVISION $ENV{provision})
-set(ROS $ENV{ros})
-set(TRACK $ENV{track})
-
-# Verify workspace location and convert to CMake path
-if(NOT DEFINED ENV{WORKSPACE})
-  fatal("ENV{WORKSPACE} was not set")
-endif()
-
-file(TO_CMAKE_PATH "$ENV{WORKSPACE}" DASHBOARD_WORKSPACE)
-
-if(NOT APPLE)
-  set(DASHBOARD_WARM_FILE "/tmp/WARM")
-  if(EXISTS "${DASHBOARD_WARM_FILE}")
-    set(DASHBOARD_WARM ON)
-    set(DASHBOARD_WARM_MESSAGE "*** This EBS volume is warm")
-  else()
-    set(DASHBOARD_WARM OFF)
-    set(DASHBOARD_WARM_MESSAGE "*** This EBS volume is cold")
-  endif()
-  message("${DASHBOARD_WARM_MESSAGE}")
-endif()
+include(${DASHBOARD_DRIVER_DIR}/environment.cmake)
 
 # Set initial configuration
 set(CTEST_TEST_ARGS "")
