2fc59ae6cf2ccb4b53c7befff14523c95053abb5
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-11-15 15:24:53 -0500

    Move provisioning, use distribution version
    Add logic to determine what UNIX distribution and version we are on, and
    use the information to select the appropriate provisioning script,
    rather than the hack of encoding this information in the compiler name.
    Move provisioning to platform.cmake.

--- driver/configurations/generic.cmake
+++ driver/configurations/generic.cmake
@@ -108,8 +108,10 @@ message("
   ROS_PACKAGE_PATH                    = $ENV{ROS_PACKAGE_PATH}
   ROS_ROOT                            = $ENV{ROS_ROOT}
   ------------------------------------------------------------------------------
-  APPLE                               = ${DASHBOARD_APPLE}
   UNIX                                = ${DASHBOARD_UNIX}
+  UNIX_DISTRIBUTION                   = ${DASHBOARD_UNIX_DISTRIBUTION}
+  UNIX_DISTRIBUTION_VERSION           = ${DASHBOARD_UNIX_DISTRIBUTION_VERSION}
+  APPLE                               = ${DASHBOARD_APPLE}
   ------------------------------------------------------------------------------
   CMAKE_VERSION                       = ${CMAKE_VERSION}
   ------------------------------------------------------------------------------
