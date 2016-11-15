982065c63194b0662dabeef3547e7a012834a6d9
Matthew Woehlke <matthew.woehlke@kitware.com>
2016-11-15 15:24:53 -0500

    Move provisioning, use distribution version
    Add logic to determine what UNIX distribution and version we are on, and
    use the information to select the appropriate provisioning script,
    rather than the hack of encoding this information in the compiler name.
    Move provisioning to platform.cmake.

--- driver/platform/unix.cmake
+++ driver/platform/unix.cmake
@@ -1,3 +1,4 @@
+# Set paths for ROS
 if(ROS AND EXISTS "/opt/ros/indigo/setup.bash")
   set(ENV{ROS_HOME} "$ENV{WORKSPACE}")
   set(ENV{ROS_ROOT} /opt/ros/indigo/share/ros)
@@ -16,6 +17,27 @@ if(ROS AND EXISTS "/opt/ros/indigo/setup.bash")
   prepend_path(CMAKE_PREFIX_PATH /opt/ros/indigo)
 endif()
 
+# Set (non-Apple) paths for MATLAB
 if(MATLAB AND NOT APPLE)
   prepend_path(PATH /usr/local/MATLAB/R2015b/bin)
+endif()
+
+# Get distribution information
+if(APPLE)
+  set(DASHBOARD_UNIX_DISTRIBUTION "Apple")
+  # TODO version
+elseif(EXISTS "/etc/os-release")
+  file(READ "/etc/os-release" DISTRIBUTION_INFO)
+  if(DISTRIBUTION_INFO MATCHES "(^|\n)NAME=\"?([^\n\"]+)\"?(\n|\$)")
+    set(DASHBOARD_UNIX_DISTRIBUTION "${CMAKE_MATCH_2}")
+  endif()
+  if(DISTRIBUTION_INFO MATCHES "(^|\n)VERSION=\"?([0-9]+([.][0-9]+)?)")
+    set(DASHBOARD_UNIX_DISTRIBUTION_VERSION "${CMAKE_MATCH_2}")
+  endif()
+  if(NOT DEFINED DASHBOARD_UNIX_DISTRIBUTION OR
+     NOT DEFINED DASHBOARD_UNIX_DISTRIBUTION_VERSION)
+    fatal("unable to determine platform distribution information")
+  endif()
+else()
+  fatal("unable to determine platform distribution information")
 endif()
