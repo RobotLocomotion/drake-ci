32b9d9298269f5932f7aef9b480ab2074b037667
Jamie Snape <jamie.snape@kitware.com>
2016-12-23 09:51:15 -0500

    Remove old catkin scripts

--- ctest_driver_script_catkin_wrapper.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-export PATH="/usr/local/bin:${PATH}"
-ctest -Dbuildname="${JOB_NAME}" -Dsite="${NODE_NAME}" -S "${WORKSPACE}/ci/ctest_driver_script_catkin.cmake" --extra-verbose --no-compress-output --output-on-failure
-[[ (-e "${WORKSPACE}/SUCCESS" || -e "${WORKSPACE}/UNSTABLE") && ! -e "${WORKSPACE}/FAILURE" ]]
