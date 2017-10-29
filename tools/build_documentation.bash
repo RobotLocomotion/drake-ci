373ff0cad1a160c4aaf4dc65f75a3274c2a25652
Jeremy Nimmer <jeremy.nimmer@tri.global>
2017-10-29 12:57:20 -0400

    Update doc targets to use non-deprecated names

--- tools/build_documentation.bash
+++ tools/build_documentation.bash
@@ -2,8 +2,8 @@
 set -euxo pipefail
 export PATH="/usr/local/bin:$PATH"
 mkdir -p build/install/share/doc/drake
-unzip bazel-genfiles/drake/doc/sphinx.zip -d build/install/share/doc/drake
-./drake/doc/doxygen.py
+unzip bazel-genfiles/doc/sphinx.zip -d build/install/share/doc/drake
+./doc/doxygen.py
 cp -r build/drake/doc/doxygen_cxx/html build/install/share/doc/drake/doxygen_cxx
 echo drake.mit.edu > build/install/share/doc/drake/CNAME
 touch build/install/share/doc/drake/.nojekyll
