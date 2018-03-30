c1de4677c5e98495e4142fc8cc237fa28e9dd9ed
Jamie Snape <jamie.snape@kitware.com>
2018-03-30 15:31:31 -0400

    Remove dead code

--- driver/configurations/common/step-report-status.cmake
+++ driver/configurations/common/step-report-status.cmake
@@ -1,12 +1,3 @@
-# Check for git errors
-# TODO: Remove when no longer trying to track down underlying cause
-if(EXISTS "${DASHBOARD_WORKSPACE}/GIT_ERROR")
-  append_step_status("GIT" UNSTABLE)
-  execute_process(
-    COMMAND /usr/bin/bash -c "ls .git-* | xargs head -n -0"
-    WORKING_DIRECTORY "${DASHBOARD_WORKSPACE}")
-endif()
-
 # Determine build result and (possibly) set status message
 if(DASHBOARD_FAILURE)
   report_status(FAILURE "FAILURE DURING %STEPS%")
