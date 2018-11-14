a64448ae568f7d6960451ea97cf2a8257bbddedc
Jamie Snape <jamie.snape@kitware.com>
2018-11-13 22:07:31 -0500

    Set Gurobi license location to /Library/gurobi800 or /opt/gurobi800

--- driver/configurations/gurobi.cmake
+++ driver/configurations/gurobi.cmake
@@ -31,22 +31,12 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-if(NOT APPLE)
+if(APPLE)
+  set(GRB_LICENSE_FILE "/Library/gurobi800/gurobi.lic")
+else()
   set(ENV{GUROBI_PATH} "/opt/gurobi800/linux64")
+  set(GRB_LICENSE_FILE "/opt/gurobi800/gurobi.lic")
 endif()
-
-set(GRB_LICENSE_FILE "${DASHBOARD_TEMP_DIR}/gurobi.lic")
-file(REMOVE "${GRB_LICENSE_FILE}")
-
-message(STATUS "Downloading Gurobi license file from AWS S3...")
-execute_process(
-  COMMAND "${DASHBOARD_AWS_COMMAND}" s3 cp
-    s3://drake-provisioning/gurobi/gurobi.lic "${GRB_LICENSE_FILE}"
-  RESULT_VARIABLE DASHBOARD_AWS_S3_RESULT_VARIABLE
-  OUTPUT_VARIABLE DASHBOARD_AWS_S3_OUTPUT_VARIABLE
-  ERROR_VARIABLE DASHBOARD_AWS_S3_OUTPUT_VARIABLE)
-list(APPEND DASHBOARD_TEMPORARY_FILES GRB_LICENSE_FILE)
-message("${DASHBOARD_AWS_S3_OUTPUT_VARIABLE}")
 
 if(NOT EXISTS "${GRB_LICENSE_FILE}")
   fatal("Gurobi license file was NOT found")
