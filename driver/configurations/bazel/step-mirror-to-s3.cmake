b7004b35272719d5ab3b98eb32fc76ff26e1ae7f
Jamie Snape <jamie.snape@kitware.com>
2019-07-02 14:02:22 -0400

    Fix location of mirror_to_s3 script

--- driver/configurations/bazel/step-mirror-to-s3.cmake
+++ driver/configurations/bazel/step-mirror-to-s3.cmake
@@ -2,7 +2,7 @@ if(DASHBOARD_FAILURE OR DASHBOARD_UNSTABLE)
   notice("CTest Status: NOT MIRRORING TO S3 BECAUSE BAZEL BUILD WAS NOT SUCCESSFUL")
 else()
   notice("CTest Status: MIRRORING TO S3")
-  execute_process(COMMAND "bazel-bin/tools/workspace/mirror_to_s3"
+  execute_process(COMMAND "${CTEST_SOURCE_DIRECTORY}/bazel-out/k8-opt/bin/tools/workspace/mirror_to_s3"
     WORKING_DIRECTORY "${CTEST_SOURCE_DIRECTORY}"
     RESULT_VARIABLE MIRROR_TO_S3_RESULT_VARIABLE)
   if(NOT MIRROR_TO_S3_RESULT_VARIABLE EQUAL 0)
