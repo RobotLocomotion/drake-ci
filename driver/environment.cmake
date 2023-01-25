fd4b6583043dc985c08c357b6a9665253f09736b
Stephen McDowell <stephen.mcdowell@kitware.com>
2023-01-25 15:23:45 -0500

    minor cleanup

--- driver/environment.cmake
+++ driver/environment.cmake
@@ -172,7 +172,6 @@ endif()
 string(REGEX MATCH "(mac-arm-monterey-clang-bazel-continuous-release|mac-arm-monterey-clang-bazel-experimental-everything-release|mac-arm-monterey-clang-bazel-experimental-release|mac-arm-monterey-clang-bazel-nightly-everything-release|mac-arm-monterey-clang-cmake-experimental-everything-release|mac-arm-monterey-clang-cmake-experimental-release|mac-arm-monterey-clang-cmake-nightly-everything-release|mac-arm-monterey-clang-cmake-nightly-release)" REGEX_MATCH_RESULT "${DASHBOARD_JOB_NAME}")
 if(REGEX_MATCH_RESULT)
   set(REMOTE_CACHE ON)
-  message("REMOTE CACHE BYPASS 1 ENABLED")
 endif()
 
 # We are also testing out running debug jobs with the new cache server.  There
@@ -180,8 +179,8 @@ endif()
 string(REGEX MATCH "(mac-arm-monterey-clang-bazel-nightly-debug|mac-arm-monterey-clang-bazel-continuous-debug|mac-arm-monterey-clang-bazel-experimental-debug)" REGEX_MATCH_RESULT "${DASHBOARD_JOB_NAME}")
 if(REGEX_MATCH_RESULT)
   set(REMOTE_CACHE ON)
-  message("REMOTE CACHE BYPASS 2 ENABLED")
 endif()
+# -- END TODO(svenevs)
 
 string(REGEX MATCH "(minsizerel|relwithdebinfo)" REGEX_MATCH_RESULT "${DASHBOARD_JOB_NAME}")
 if(REGEX_MATCH_RESULT)
