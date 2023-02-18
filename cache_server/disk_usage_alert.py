18859531fee84960517522c74389b5c86cd8caed
Stephen McDowell <stephen.mcdowell@kitware.com>
2023-02-17 19:47:54 -0500

    cleanup / reduce clutter
    this script no longer emails, and soon it will not be getting run
    by cron but instead via jenkins login

--- cache_server/disk_usage_alert.py
+++ cache_server/disk_usage_alert.py
@@ -1,45 +1,30 @@
 #!/usr/bin/env python3
-"""Gather usage statistics on the cache server volume utilization, emailing
-developers when the cache server utilization is too high.
+"""Gather usage statistics on the cache server volume utilization.
 
-This script is expected to be run via /etc/crontab daily, as `root`.  The
-/etc/ssmtp/ssmtp.conf must already must already be configured, see the README for more:
-https://github.com/RobotLocomotion/drake-ci/tree/main/cache_server
-
-For cron jobs, provide the argument ``--log-file /path/to/desired.log`` so that
-administrators can review the scripts automated actions.  Use ``--log-console`` to
-instead have output print to the console.  One of ``--log-file`` or ``--log-console``
-must be provided.  Usage of ``--log-console`` via ``cron`` is discouraged, any output
-to stdout / stderr in cron will get emailed (to nobody) via ``ssmtp``.
+This script is expected to be run via /etc/crontab daily, as `root`.  Its output should
+be redirected to a logfile.
 """
 
 import argparse
-from datetime import datetime, timezone
 import logging
 import os
 from pathlib import Path
 import shutil
-import subprocess
+import sys
 from textwrap import dedent
 
 
 def log(
     msg: str,
-    *,
-    to_console: bool,
 ):
-    """Log ``msg`` to the console, or log each line in ``msg`` (individually) to
-    ``logging.warning``.  Assumes ``logging.basicConfig`` has already been
-    configured.
+    """Log each line in ``msg`` (individually) to ``logging.warning``.  Assumes
+    ``logging.basicConfig`` has already been configured.
 
     In the log files we want an organized structure with all messages preceded
-    by their date.  When logging to the console, this information is less useful.
+    by their date.
     """
-    if to_console:
-        print(msg)
-    else:
-        for line in msg.splitlines():
-            logging.warning(line)
+    for line in msg.splitlines():
+        logging.warning(line)
 
 
 def main() -> None:
@@ -47,47 +32,11 @@ def main() -> None:
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
     )
     parser.add_argument(
-        "-n",
-        "--name",
-        type=str,
-        required=True,
-        choices=[
-            "mac-arm64"
-            # TODO(svenevs): add in the other cache servers when they are alive.
-            # "mac-x86",
-            # "linux",
-        ],
-        help="The name of the cache server (used in emails).",
-    )
-    parser.add_argument(
         "-t",
         "--threshold",
         type=float,
-        default=80.0,
+        default=85.0,
         help="Threshold to send an email alert if disk usage exceeds this value.",
-    )
-    log_group = parser.add_mutually_exclusive_group(required=True)
-    log_group.add_argument(
-        "--log-file",
-        dest="log_file",
-        type=Path,
-        help="File to log to.  If the file exists, it will be appended to.",
-    )
-    log_group.add_argument(
-        "--log-console",
-        dest="log_console",
-        action="store_true",
-        help="Log to the console rather than to a file.",
-    )
-    parser.add_argument(
-        "-e",
-        "--emails",
-        nargs="+",
-        default=[
-            "drake-alerts+jenkins@tri.global",
-            "drake-developers+build-cop@kitware.com",
-        ],
-        help="Where to send emails to if disk usage is too high.",
     )
     parser.add_argument(
         "mount_point",
@@ -124,17 +73,12 @@ def main() -> None:
         )
 
     # Configure the logging format *before* any `log` calls are issued.
-    basic_config_kwargs = dict(
+    logging.basicConfig(
         format="%(asctime)s :: %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
     )
-    if not args.log_console:
-        basic_config_kwargs["filename"] = args.log_file
-        basic_config_kwargs["filemode"] = "a"  # append is default, but be explicit
-    logging.basicConfig(**basic_config_kwargs)  # type: ignore[arg-type]
 
-    # When logging to a file, make it easy for the reviewer to find separate entries.
-    if not args.log_console:
-        log("=" * 80, to_console=args.log_console)
+    # Make it easy for the reviewer to find separate entries in the logfile.
+    log("=" * 80)
 
     # Compute and report the data / statistics we care about.  All of our cache servers
     # operate on the order of gigabytes, so there is no need to be able to detect
@@ -153,89 +97,32 @@ def main() -> None:
         - %Used: {round(percent_used, 2)}%
     """
     )
-    log(mount_data, to_console=args.log_console)
+    log(mount_data)
 
     if percent_used < args.threshold:
         # Simply report back to the logs everything is as expected.
         log(
             f"\n==> {percent_used}% usage is adequately beneath {args.threshold}%!",
-            to_console=args.log_console,
         )
     else:
-        # Different servers are in different regions, primarily operated in US/Eastern
-        # but some of them have their clocks set to UTC.
-        now = datetime.now(timezone.utc)
-        # NOTE: include the year-month-day hour:minute in subject so that each alert
-        # will start a new "email thread".
-        subject = f"Cache Overutilized {now.strftime('%Y-%m-%d %H:%M:%S UTC')}"
-
-        # NOTE: do not word-wrap sentences to the next line, it looks weird in the
-        # actual email body.  This `last_sentence` is to be all on one line, but for
-        # code line-length purposes we define it this way.
-        last_sentence = (
-            "Please start a thread in the #buildcop slack channel "
-            "https://drakedevelopers.slack.com/archives/C270MN28G delegating to "
-            "Kitware, or follow the instructions at "
-            "https://github.com/RobotLocomotion/drake-ci/tree/main/cache_server to "
-            "cleanup manually."
-        )
-        cache_server_name = f"Drake {args.name} Cache Server"
-        # ssmtp parses for the line `Subject: ...` for the email subject.
-        # NOTE: `mount_data` needs to be formatted *after* dedent.
-        ssmtp_email = dedent(
-            f"""\
-            Subject: {subject}
-
-            The {cache_server_name} disk usage is too high:
-            {{mount_data}}
-            {percent_used}% exceeds the provided threshold of {args.threshold}%.
-
-            {last_sentence}
-        """
-        ).format(mount_data=mount_data)
         log(
-            f"\n(!) Emailing alert to: {', '.join(args.emails)} (!)\n\n",
-            to_console=args.log_console,
-        )
-        log(ssmtp_email, to_console=args.log_console)
+            dedent(
+                f"""
+            [X] The cache server disk usage is too high:
 
-        ssmtp_args = [
-            "ssmtp",
-            "-F",
-            cache_server_name,
-            *args.emails,
-        ]
-        try:
-            proc = subprocess.run(
-                ssmtp_args,
-                capture_output=True,
-                input=bytes(ssmtp_email, "utf-8"),
+            {percent_used}% usage exceeds the provided threshold of {args.threshold}%!
+
+            Please start a thread in the #buildcop slack channel delegating to Kitware:
+
+            https://drakedevelopers.slack.com/archives/C270MN28G
+
+            Or follow the instructions to cleanup manually:
+
+            https://github.com/RobotLocomotion/drake-ci/tree/main/cache_server
+        """
             )
-            if proc.returncode != 0:
-                log(
-                    dedent(
-                        f"""
-                        ERROR:
-                          Nonzero exit code of {proc.returncode} running {ssmtp_args}
-                          {proc.stdout.decode('ascii')}
-                          {proc.stderr.decode('ascii')}
-                    """
-                    ),
-                    to_console=args.log_console,
-                )
-            else:
-                log(f"\nSUCCESS: Alert sent: {ssmtp_args}", to_console=args.log_console)
-        except Exception as e:
-            log(
-                dedent(
-                    f"""
-                    ERROR:
-                      Unable to execute {ssmtp_args}:
-                      {e}
-                """
-                ),
-                to_console=args.log_console,
-            )
+        )
+        sys.exit(1)
 
 
 if __name__ == "__main__":
