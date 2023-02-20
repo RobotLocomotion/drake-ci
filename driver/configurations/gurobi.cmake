23fec50dc8c8eb046c1ebd27b0642140f7fcea9f
Stephen McDowell <stephen.mcdowell@kitware.com>
2023-02-20 12:20:00 -0500

    Restore "Run Gurobi tests in parallel (#194)"
    Restore #194 by reverting its revert commit (#199).
    
    This reverts commit e0f93e61ea00b6419ede0fb71f7c986ed3dfeb71.

--- driver/configurations/gurobi.cmake
+++ driver/configurations/gurobi.cmake
@@ -61,3 +61,6 @@ endif()
 
 # Always set environment variable so remote caches may be shared.
 set(ENV{GRB_LICENSE_FILE} "${GRB_LICENSE_FILE}")
+
+# Run Gurobi tests in parallel in the CI
+set(ENV{DRAKE_GUROBI_LICENSE_UNLIMITED} 1)
