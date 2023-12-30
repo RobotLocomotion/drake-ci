2cc8c8318ed5a5266a083757bff96ff5bcedd56d
Stephen McDowell <stephen.mcdowell@kitware.com>
2023-12-30 18:42:25 -0500

    [ci] Remove relaxed SSH permissions
    Related to other jenkins and java keystore SSL cert changes on
    the backend.

--- driver/configurations/aws.cmake
+++ driver/configurations/aws.cmake
@@ -68,6 +68,52 @@ else()
   chmod("${SSH_PRIVATE_KEY_FILE}" 0400 "private key file")
 endif()
 
+# Download our private known_hosts file and make it available to workers.
+# It includes hosts for GitHub and more, see tools/generate_known_hosts.py.
+set(SSH_USER_DIRECTORY "$ENV{HOME}/.ssh")
+# NOTE: there are problems downloading directly to ~/.ssh, download first then
+# move the file to the right place.
+set(SSH_KNOWN_HOSTS_DESTINATION_PATH "${SSH_USER_DIRECTORY}/known_hosts")
+mkdir("${SSH_USER_DIRECTORY}" 0700 "${SSH_USER_DIRECTORY}")
+# Apple is special: https://apple.stackexchange.com/a/319743
+# This appears to be why we cannot download directly to ~/.ssh/known_hosts.
+if(APPLE)
+  # Things seem to go wrong when the file is first created.
+  set(xattr_locations "${SSH_USER_DIRECTORY}")
+  foreach(location IN LISTS xattr_locations)
+    message(STATUS  "Running xattr -c ${location}")
+    execute_process(COMMAND
+      sudo xattr -c "${location}"
+      RESULT_VARIABLE _xattr_result
+      OUTPUT_VARIABLE _xattr_output
+      ERROR_VARIABLE _xattr_error)
+    if(NOT _xattr_result EQUAL 0)
+      fatal(
+        "Unable to xattr -c ${location}: "
+        "${_xattr_output} ${_xattr_error}.")
+    endif()
+  endforeach()
+endif()
+
+message(STATUS "Downloading private known_hosts from AWS S3...")
+execute_process(
+  COMMAND "${DASHBOARD_AWS_COMMAND}" s3 cp
+    s3://drake-provisioning/drake_known_hosts
+    "${SSH_KNOWN_HOSTS_DESTINATION_PATH}"
+  RESULT_VARIABLE DASHBOARD_AWS_S3_RESULT_VARIABLE)
+if(NOT DASHBOARD_AWS_S3_RESULT_VARIABLE EQUAL 0)
+  fatal("download of private known_hosts from AWS S3 was not successful")
+endif()
+chmod("${SSH_KNOWN_HOSTS_DESTINATION_PATH}" 0600 "Creating ${SSH_KNOWN_HOSTS_DESTINATION_PATH}")
+
+# Verify the expected SHA-1 of the private key file
+file(SHA1 "${SSH_KNOWN_HOSTS_DESTINATION_PATH}" SSH_KNOWN_HOSTS_DESTINATION_PATH_SHA1)
+if(NOT SSH_KNOWN_HOSTS_DESTINATION_PATH_SHA1 STREQUAL "3d935ef12d588e6e090de122ab8d9198d263e219")
+  fatal("SHA-1 of private key file was not correct"
+  SSH_KNOWN_HOSTS_DESTINATION_PATH_SHA1)
+endif()
+
+
 # Add private key to agent
 if(NOT SSH_PRIVATE_KEY_FILE STREQUAL "-")
   message(STATUS "Adding private key to ssh-agent")
@@ -78,7 +124,4 @@ if(NOT SSH_PRIVATE_KEY_FILE STREQUAL "-")
   if(NOT DASHBOARD_SSH_AGENT_RESULT_VARIABLE EQUAL 0)
     fatal("adding private key to ssh-agent was not successful")
   endif()
-
-  # Forward the credentials.
-  set(ENV{GIT_SSH_COMMAND} "ssh -o StrictHostKeyChecking=no -i '${SSH_PRIVATE_KEY_FILE}'")
 endif()
