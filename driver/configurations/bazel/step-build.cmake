cfa7c00f5fd9e9a446ab52a2d81770e9e387cd6f
Stephen McDowell <stephen.mcdowell@kitware.com>
2024-03-06 11:18:34 -0500

    [ctest] Fix warnings being detected as errors

--- driver/configurations/bazel/step-build.cmake
+++ driver/configurations/bazel/step-build.cmake
@@ -62,7 +62,13 @@ else()
     "${DASHBOARD_BAZEL_STARTUP_OPTIONS} test ${DASHBOARD_BAZEL_BUILD_OPTIONS} ${DASHBOARD_BAZEL_TEST_OPTIONS} ...")
 endif()
 
-set(CTEST_CUSTOM_ERROR_EXCEPTION "^WARNING: " ":[0-9]+: Failure$")
+set(CTEST_CUSTOM_ERROR_EXCEPTION
+  "^WARNING: "
+  ":[0-9]+: Failure$"
+  # Issues related to macOS updates
+  # https://drakedevelopers.slack.com/archives/C270MN28G/p1709736230811259?thread_ts=1709736131.932479&cid=C270MN28G
+  "SyntaxWarning: invalid escape sequence '\\s'"
+  )
 set(CTEST_CUSTOM_ERROR_MATCH "^ERROR: " "^FAIL: " "^TIMEOUT: ")
 set(CTEST_CUSTOM_WARNING_MATCH "^WARNING: ")
 
