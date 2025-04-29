ac56f3e56e35a231d36a53974c89cbc4ae549bf9
tyler-yankee <tyler.yankee@kitware.com>
2025-04-29 07:12:40 -0400

    Revert "Re-enable --remote_download_outputs=minimal (#323)"
    This reverts commit 373c1aa80e91c6318964a1514faa978290712937.

--- driver/configurations/cmake.cmake
+++ driver/configurations/cmake.cmake
@@ -120,8 +120,7 @@ include(${DASHBOARD_DRIVER_DIR}/configurations/cache.cmake)
 if(REMOTE_CACHE)
   file(APPEND "${DASHBOARD_SOURCE_DIRECTORY}/user.bazelrc"
     "build --experimental_guard_against_concurrent_changes=yes\n"
-    "build --remote_download_outputs=minimal\n"
-    "build --experimental_remote_cache_lease_extension=yes\n"
+    "build --remote_download_outputs=all\n"
     "build --remote_cache=${DASHBOARD_REMOTE_CACHE}\n"
     "build --remote_local_fallback=yes\n"
     "build --remote_max_connections=64\n"
