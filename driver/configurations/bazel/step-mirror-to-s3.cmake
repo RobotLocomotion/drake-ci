516d426a8012da27cfb578e7225c7f3e6b829a7f
Tyler Yankee <tyler.yankee@kitware.com>
2026-03-03 10:15:17 -0500

    Run mirror-to-s3 with `--no-upload` under non-continuous jobs
    Experimental mirror-to-s3 should still provide some verification by
    running the script, even if it shouldn't actually upload anything.
    
    This prepares for a future change to the mirror-to-s3 jobs to also
    mirror the most recent release, for which we'll want to reuse a similar
    'no-upload' / 'dry-run' functionality on experimental variants.

--- driver/configurations/bazel/step-mirror-to-s3.cmake
+++ driver/configurations/bazel/step-mirror-to-s3.cmake
@@ -37,6 +37,9 @@ if(DASHBOARD_FAILURE OR DASHBOARD_UNSTABLE)
 else()
   notice("CTest Status: MIRRORING TO S3")
   set(MIRROR_TO_S3_CMD "bazel-bin/tools/workspace/mirror_to_s3")
+  if(NOT MIRROR_TO_S3 STREQUAL "publish")
+    string(APPEND MIRROR_TO_S3_CMD " --no-upload")
+  endif()
   notice("Mirror to S3 command: ${MIRROR_TO_S3_CMD}")
   execute_process(COMMAND "${MIRROR_TO_S3_CMD}"
     WORKING_DIRECTORY "${CTEST_SOURCE_DIRECTORY}"
