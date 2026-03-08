b6f364616ed07a2001553a0cf2a64109a3cf82ff
Tyler Yankee <tyler.yankee@kitware.com>
2026-03-08 18:37:55 -0400

    Add release artifact mirroring to Mirror to S3 job

--- driver/configurations/bazel/step-mirror-to-s3.cmake
+++ driver/configurations/bazel/step-mirror-to-s3.cmake
@@ -35,16 +35,29 @@
 if(DASHBOARD_FAILURE OR DASHBOARD_UNSTABLE)
   notice("CTest Status: NOT MIRRORING TO S3 BECAUSE BAZEL BUILD WAS NOT SUCCESSFUL")
 else()
-  notice("CTest Status: MIRRORING TO S3")
-  set(MIRROR_TO_S3_CMD "bazel-bin/tools/workspace/mirror_to_s3")
+  notice("CTest Status: MIRRORING SOURCE DEPENDENCY ARCHIVES TO S3")
+  set(MIRROR_TO_S3_WORKSPACE_CMD "bazel-bin/tools/workspace/mirror_to_s3")
   if(NOT MIRROR_TO_S3 STREQUAL "publish")
-    list(APPEND MIRROR_TO_S3_CMD "--no-upload")
+    list(APPEND MIRROR_TO_S3_WORKSPACE_CMD "--no-upload")
   endif()
-  notice("Mirror to S3 command: ${MIRROR_TO_S3_CMD}")
-  execute_process(COMMAND ${MIRROR_TO_S3_CMD}
+  notice("Mirror to S3 workspace command: ${MIRROR_TO_S3_WORKSPACE_CMD}")
+  execute_process(COMMAND ${MIRROR_TO_S3_WORKSPACE_CMD}
     WORKING_DIRECTORY "${CTEST_SOURCE_DIRECTORY}"
-    RESULT_VARIABLE MIRROR_TO_S3_RESULT_VARIABLE)
-  if(NOT MIRROR_TO_S3_RESULT_VARIABLE EQUAL 0)
-    append_step_status("BAZEL MIRRORING TO S3" UNSTABLE)
+    RESULT_VARIABLE MIRROR_TO_S3_WORKSPACE_RESULT_VARIABLE)
+  if(NOT MIRROR_TO_S3_WORKSPACE_RESULT_VARIABLE EQUAL 0)
+    append_step_status("BAZEL MIRRORING SOURCE DEPENDENCY ARCHIVES TO S3" UNSTABLE)
+  endif()
+
+  notice("CTest Status: MIRRORING RELEASE ARTIFACTS TO S3")
+  set(MIRROR_TO_S3_RELEASE_CMD "bazel-bin/tools/release_engineering/mirror_to_s3")
+  if(NOT MIRROR_TO_S3 STREQUAL "publish")
+    list(APPEND MIRROR_TO_S3_RELEASE_CMD "--dry-run")
+  endif()
+  notice("Mirror to S3 release command: ${MIRROR_TO_S3_RELEASE_CMD}")
+  execute_process(COMMAND ${MIRROR_TO_S3_RELEASE_CMD}
+    WORKING_DIRECTORY "${CTEST_SOURCE_DIRECTORY}"
+    RESULT_VARIABLE MIRROR_TO_S3_RELASE_RESULT_VARIABLE)
+  if(NOT MIRROR_TO_S3_RELASE_RESULT_VARIABLE EQUAL 0)
+    append_step_status("BAZEL MIRRORING RELEASE ARTIFACTS TO S3" UNSTABLE)
   endif()
 endif()
