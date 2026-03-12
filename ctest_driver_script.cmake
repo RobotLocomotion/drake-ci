5b3d11d53045b8927ce51a400a6423ff6848d245
Betsy McPhail <betsy.mcphail@kitware.com>
2026-03-12 13:04:36 -0400

    Remove Jammy as a supported CI distribution (#384)
    Also remove Jammy-specific workarounds and other mentions.
    
    Update CI cmake_minimum_required to 3.28.

--- ctest_driver_script.cmake
+++ ctest_driver_script.cmake
@@ -49,7 +49,7 @@
 #   ENV{USER}                  required
 #   ENV{WORKSPACE}             required
 
-cmake_minimum_required(VERSION 3.22)
+cmake_minimum_required(VERSION 3.28)
 
 set(CTEST_RUN_CURRENT_SCRIPT OFF)  # HACK
 
