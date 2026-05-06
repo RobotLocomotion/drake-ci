588970780487840112da237608f8a75171d0053a
Tyler Yankee <tyler.yankee@kitware.com>
2026-05-06 14:04:15 -0400

    Standardize binary tarball architecture naming
    The convention established for Ubuntu arm64 packages in commit d3244f6
    to use `uname -m` is no longer sufficient in the context of
    architecture variants as of Ubuntu 26.04 and beyond, since this command
    doesn't indicate the variant but the resulting binaries are
    ABI-incompatible between variants.
    
    Instead, re-spell our binary tarballs to respect `dpkg` conventions,
    and incorporate the variant information, following the approach from
    commit 867a1e6. This also results in consistent naming between the
    tarballs and `.deb` packages.
    
    Noble x86_64 binaries are untouched for compatibility.

--- driver/architecture.cmake
+++ driver/architecture.cmake
@@ -1,19 +1,20 @@
 # -*- mode: cmake; -*-
 # vi: set ft=cmake:
 
-if(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "x86_64")
-  # Check we have an architecture variant enabled; if not, the output is empty.
+if(NOT APPLE)
   execute_process(
-      COMMAND "dpkg-query" "--show" "--showformat=\${Architecture-Variant}" "libc6"
-      OUTPUT_VARIABLE DASHBOARD_DEB_ARCH
-      COMMAND_ECHO NONE
-      OUTPUT_STRIP_TRAILING_WHITESPACE)
-  if(NOT DASHBOARD_DEB_ARCH)
-    set(DASHBOARD_DEB_ARCH "amd64")
+    COMMAND
+      "dpkg-query" "--show"
+      [==[--showformat=${Architecture-Variant}\n${Architecture}\n]==]
+      "libc6"
+    COMMAND
+      "grep" "-m" "1" "."
+    OUTPUT_VARIABLE DASHBOARD_DEB_ARCH
+    RESULT_VARIABLE DASHBOARD_DEB_ARCH_RESULT
+    ERROR_VARIABLE DASHBOARD_DEB_ARCH_ERROR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+  if (NOT DASHBOARD_DEB_ARCH_RESULT EQUAL 0)
+    fatal("Unable to determine architecture: ${DASHBOARD_DEB_ARCH_ERROR}")
   endif()
-elseif(CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "arm64" OR
-  CMAKE_HOST_SYSTEM_PROCESSOR STREQUAL "aarch64")
-  set(DASHBOARD_DEB_ARCH "arm64")
-else()
-  fatal("Unable to determine architecture, or unsupported: ${CMAKE_HOST_SYSTEM_PROCESSOR}")
 endif()
