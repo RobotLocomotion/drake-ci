2c2263f711e4a29351751469832730bc24fa65b2
Tyler Yankee <tyler.yankee@kitware.com>
2026-05-07 15:27:48 -0400

    Update CTest groups (#427)
    * Reduce duplication between DASHBOARD_MODEL, TRACK, and
      DASHBOARD_TRACK, including avoiding confusion over capitalization
      (now, we only ever operate lower case).
    * Update the "track" terminology to "group"; the former was deprecated
      and retained for backwards compatibility only in CTest 3.16.
    * Remove a stale reference to the old weekly builds. Since staging
      builds never upload to CDash, this means we now have a 1:1 mapping
      of "groups" to CTest models (experimental, continuous, nightly).

--- driver/configurations/wheel/step-upload-pip-index-url.cmake
+++ driver/configurations/wheel/step-upload-pip-index-url.cmake
@@ -4,7 +4,7 @@
 # This step should only be executing after a nightly wheel build (for any
 # distribution) is complete.  Experimental / staging builds should NOT
 # regenerate the pip nightly index.
-if(DASHBOARD_TRACK STREQUAL "Nightly")
+if(DASHBOARD_GROUP STREQUAL "nightly")
   if(DASHBOARD_FAILURE OR DASHBOARD_UNSTABLE)
     notice("CTest Status: NOT GENERATING PIP INDEX URL BECAUSE WHEEL BUILD WAS NOT SUCCESSFUL")
   else()
