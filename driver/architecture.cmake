497e3f1acacfc2acb4417fd930c1fea7c175c150
Tyler Yankee <tyler.yankee@kitware.com>
2026-05-10 17:55:15 -0400

    Support amd64v1 architecture variant on Resolute
    To avoid excess maintenance of separate EC2 images, we'll have the
    unprovisioned job logic "reset" the currently-running instance to
    the appropriate architecture variant by removing the v3 config file.

--- driver/architecture.cmake
+++ driver/architecture.cmake
@@ -2,11 +2,12 @@
 # vi: set ft=cmake:
 
 if(NOT APPLE)
-  # Use the first of Architecture-Variant or Architecture that is defined,
-  # since the former isn't always available/applicable.
+  # Determine the architecture/variant.
   execute_process(
     COMMAND
       "dpkg-query" "--show"
+      # Use the first of Architecture-Variant or Architecture that is defined,
+      # since the former isn't always available/applicable.
       [==[--showformat=${Architecture-Variant}\n${Architecture}\n]==]
       "libc6"
     COMMAND
@@ -18,5 +19,8 @@ if(NOT APPLE)
   )
   if (NOT DASHBOARD_DEB_ARCH_RESULT EQUAL 0 OR DASHBOARD_DEB_ARCH STREQUAL "")
     fatal("Unable to determine architecture: ${DASHBOARD_DEB_ARCH_ERROR}")
+  endif()
+  if (NOT DASHBOARD_DEB_ARCH STREQUAL ARCH_AND_VARIANT)
+    fatal("Failed to set the intended architecture variant: got ${DASHBOARD_DEB_ARCH}, expected ${ARCH_AND_VARIANT}")
   endif()
 endif()
