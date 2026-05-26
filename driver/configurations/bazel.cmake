5b4ac3d71a9bde907a0fc253f114699b0548636a
Tyler Yankee <tyler.yankee@kitware.com>
2026-05-26 09:19:08 -0400

    Reorganize rcfiles and factor out remote cache setup

--- driver/configurations/bazel.cmake
+++ driver/configurations/bazel.cmake
@@ -41,26 +41,6 @@ else()
   set(DASHBOARD_JOBS 1)
 endif()
 
-include(${DASHBOARD_DRIVER_DIR}/configurations/cache.cmake)
-
-if(REMOTE_CACHE)
-  if(DEBUG)
-    set(DASHBOARD_REMOTE_MAX_CONNECTIONS 16)
-    set(DASHBOARD_REMOTE_RETRIES 1)
-    set(DASHBOARD_REMOTE_TIMEOUT 240)
-  else()
-    set(DASHBOARD_REMOTE_MAX_CONNECTIONS 64)
-    set(DASHBOARD_REMOTE_RETRIES 4)
-    set(DASHBOARD_REMOTE_TIMEOUT 120)
-  endif()
-  configure_file("${DASHBOARD_TOOLS_DIR}/remote.bazelrc.in"
-    "${CTEST_SOURCE_DIRECTORY}/remote.bazelrc" @ONLY
-  )
-  configure_file("${DASHBOARD_TOOLS_DIR}/remote-v9.1.bazelrc.in"
-    "${CTEST_SOURCE_DIRECTORY}/remote-v9.1.bazelrc" @ONLY
-  )
-endif()
-
 set(DASHBOARD_BAZEL_BUILD_OPTIONS)
 
 # The only time we use a non-default compiler is the Clang variant on Ubuntu.
