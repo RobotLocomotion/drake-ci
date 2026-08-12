4d478290e723cba5379339f695e9177d2c2783bb
Tyler Yankee <tyler.yankee@kitware.com>
2026-08-12 12:44:41 -0400

    Add a dry-run mode to pip_index_url
    We can now run this under the non-nightly flavors (and just avoid the
    actual S3 update), improving consistency and catching errors sooner.

--- driver/configurations/wheel/step-upload-pip-index-url.cmake
+++ driver/configurations/wheel/step-upload-pip-index-url.cmake
@@ -1,14 +1,9 @@
 # -*- mode: cmake; -*-
 # vi: set ft=cmake:
 
-# This step should only be executing after a nightly wheel build (for any
-# distribution) is complete.  Experimental / staging builds should NOT
-# regenerate the pip nightly index.
-if(DASHBOARD_GROUP STREQUAL "nightly")
-  if(DASHBOARD_FAILURE OR DASHBOARD_UNSTABLE)
-    notice("CTest Status: NOT GENERATING PIP INDEX URL BECAUSE WHEEL BUILD WAS NOT SUCCESSFUL")
-  else()
-    notice("CTest Status: GENERATING PIP INDEX URL")
-    generate_pip_index_url()
-  endif()
+if(DASHBOARD_FAILURE OR DASHBOARD_UNSTABLE)
+  notice("CTest Status: NOT GENERATING PIP INDEX URL BECAUSE WHEEL BUILD WAS NOT SUCCESSFUL")
+else()
+  notice("CTest Status: GENERATING PIP INDEX URL")
+  generate_pip_index_url()
 endif()
